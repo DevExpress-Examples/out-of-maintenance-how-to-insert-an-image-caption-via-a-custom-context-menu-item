264b1b1052daa409396f600a4ad2ae6136a84378
CORP\alakina.svetlana <svetlana.rebrova@devexpress.com>
2018-07-23 15:46:13 +0300

    add example content

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using DevExpress.Web.Office;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            DocumentManager.CloseAllDocuments();
+            ASPxRichEdit1.Open(Server.MapPath("TextWithImages.rtf"));
+        }
+    }
+}
